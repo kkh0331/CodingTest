ef1161a27dcca5f94b395fa06cf0e0dffe5d4313
kihyun.kwon <kkh3272@naver.com>
2024-01-29 22:42:08 +0900

    feat: baekjoon_9372.java 파일 추가

---
+++ src/tree/baekjoon_9372.java
@@ -0,0 +1,21 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_9372 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+            }
+            System.out.println(N-1);
+        }
+    }
+}
